e562f7bb2554c2991c9e21bbf1c4718da2b68773
simon1901 <77052619+simon1901@users.noreply.github.com>
2021-01-16 16:16:23 +0100

    This is a summary.

---
+++ Modern.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="Modern_2" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="Modern_2.png" width="512" height="512"/>
+ <tile id="51">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
